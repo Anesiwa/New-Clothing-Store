2acc21047b1b5ebe5507a07ce59aea2531af1996
Anesiwa <89821512+Anesiwa@users.noreply.github.com>
2022-05-16 07:52:09 -0500

    Ran debugger and updated Posts page

--- react-multi-page-website/src/components/blog/Posts.jsx
+++ react-multi-page-website/src/components/blog/Posts.jsx
@@ -37,10 +37,11 @@ function Posts() {
             <div className="col-lg-5">
               <h1 className="font-weight-light">Homemade Cheesecake Menu</h1>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                Home is where the cheesemake is! orem Ipsum is simply dummy text
+                of the printing and typesetting industry. Lorem Ipsum has been
+                the industry's standard dummy text ever since the 1500s, when an
+                unknown printer took a galley of type and scrambled it to make a
+                type specimen book.
               </p>
             </div>
           </div>
