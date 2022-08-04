7b3d0647aef9c026716af3bbe292cd69087e1c61
Anesiwa <89821512+Anesiwa@users.noreply.github.com>
2022-08-03 19:43:51 -0500

    Swapped out older version of Navbar with responsive hamburger menu that also includes a register and log-in section.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,49 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./Navigation.css";
-import logo from "./images/phoenix_bird_blk.png";
+import { Navbar } from "./navbar/navbar";
 
 export default function Navigation() {
-  return (
-    <div>
-      <nav className="navbar navigation">
-        <span className="float left">
-          <img
-            className="img-fluid"
-            src={logo}
-            alt="Embers of the Phoenix logo"
-            width="60"
-          />
-        </span>
-        <ul className="navbar-nav navbar-expand-lg">
-          <li className="nav-item">
-            <NavLink className="nav-button" to="/">
-              HOME
-              <span className="sr-only">(current)</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-button" to="/about">
-              ABOUT
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-button" to="/contact">
-              CONTACT
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-button" to="/blog">
-              MENUS
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-button" to="./SpecialEvents">
-              SPECIAL EVENTS
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+  return <Navbar />;
 }
